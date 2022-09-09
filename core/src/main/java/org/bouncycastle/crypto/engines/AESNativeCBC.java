8c0577916c9bbaab1ccf543b30bd46396a78e313
mw <megan@cryptoworkshop.com>
2022-09-09 19:19:38 +1000

    CBC logic

--- core/src/main/java/org/bouncycastle/crypto/engines/AESNativeCBC.java
+++ core/src/main/java/org/bouncycastle/crypto/engines/AESNativeCBC.java
@@ -80,7 +80,7 @@ public class AESNativeCBC
         }
 
 
-        if (key == null || oldEncrypting != encrypting)
+        if (key == null && oldEncrypting != encrypting)
         {
             throw new IllegalArgumentException("cannot change encrypting state without providing key.");
         }
