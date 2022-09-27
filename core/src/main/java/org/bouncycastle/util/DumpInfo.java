c74c959734d12a209bceecfcebb2539948b747eb
mw <megan@cryptoworkshop.com>
2022-09-27 15:47:59 +1000

    Wrong main signature

--- core/src/main/java/org/bouncycastle/util/DumpInfo.java
+++ core/src/main/java/org/bouncycastle/util/DumpInfo.java
@@ -5,7 +5,7 @@ import org.bouncycastle.crypto.NativeServices;
 
 public class DumpInfo
 {
-    public static void main(String args)
+    public static void main(String[] args)
     {
         CryptoServicesRegistrar.getNativeServices();
 
