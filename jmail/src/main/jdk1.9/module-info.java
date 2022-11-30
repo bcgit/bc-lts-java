96dc7311c93343eea5cef69f6ff9151361220af9
David Hook <david.hook@keyfactor.com>
2022-11-30 19:39:37 +1100

    jmail with gradle build

---
+++ jmail/src/main/jdk1.9/module-info.java
@@ -0,0 +1,13 @@
+module org.bouncycastle.mail
+{
+    requires org.bouncycastle.lts.prov;
+    requires transitive org.bouncycastle.lts.pkix;
+    requires jakarta.mail;
+    requires jakarta.activation;
+
+    exports org.bouncycastle.mail.smime;
+    exports org.bouncycastle.mail.smime.examples;
+    exports org.bouncycastle.mail.smime.handlers;
+    exports org.bouncycastle.mail.smime.util;
+    exports org.bouncycastle.mail.smime.validator;
+}
