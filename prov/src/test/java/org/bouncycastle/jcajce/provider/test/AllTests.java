c7f5c8ba4693bdc63939202f5e989ba5522dc5c4
David Hook <dgh@cryptoworkshop.com>
2023-04-16 17:20:59 +1000

    added use of TestResourceFinder, cleaned up test utilities.

--- prov/src/test/java/org/bouncycastle/jcajce/provider/test/AllTests.java
+++ prov/src/test/java/org/bouncycastle/jcajce/provider/test/AllTests.java
@@ -6,15 +6,15 @@ import junit.extensions.TestSetup;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.bouncycastle.test.prov.PrintResults;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.test.PrintTestResult;
 
 public class AllTests
     extends TestCase
 {
     public static void main (String[] args)
     {
-       PrintResults.printResult( junit.textui.TestRunner.run(suite()));
+       PrintTestResult.printResult(junit.textui.TestRunner.run(suite()));
     }
     
     public static Test suite()
