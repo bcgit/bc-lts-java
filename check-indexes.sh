4cf3a3dfec3b846676ba05eb12ff57bf2a4006d1
David Hook <dgh@cryptoworkshop.com>
2023-04-16 21:27:30 +1000

    added index for util

--- check-indexes.sh
+++ check-indexes.sh
@@ -1,4 +1,7 @@
 #/bin/sh
+#
+# Check which files have changed in bc-java since we last looked.
+#
 
 if [ $# != 1 ]
 then
@@ -18,3 +21,14 @@ do
         echo $file
     fi
 done < indexes/bc-java.core.index
+
+while read l
+do
+    hash=`echo $l | sed -e "s/ .*//"`
+    file=`echo $l | sed -e "s/^[^ ]* //"`
+    current=`sha256sum $bc_java_dir/$file |  sed -e "s/ .*//"`
+    if [ $hash != $current ]
+    then
+        echo $file
+    fi
+done < indexes/bc-java.util.index
