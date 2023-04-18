4c0dfeb44e6d3644422981696dfadfe53ca1c92e
David Hook <dgh@cryptoworkshop.com>
2023-04-18 12:14:33 +1000

    index update, minor refactor

--- pkix/src/test/java/org/bouncycastle/cert/test/BcCertTest.java
+++ pkix/src/test/java/org/bouncycastle/cert/test/BcCertTest.java
@@ -23,7 +23,6 @@ import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1Enumerated;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.asn1.DERBitString;
 import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
