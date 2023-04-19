b19bb26e934aef565804462c1dcae15688062f4f
mwcw <megan@cryptoworkshop.com>
2023-04-19 11:19:02 +1000

    Removed exported package that does not exist.

--- mail/src/main/jdk1.9/module-info.java
+++ mail/src/main/jdk1.9/module-info.java
@@ -5,7 +5,7 @@ module org.bouncycastle.lts.mail
     requires transitive org.bouncycastle.lts.pkix;
 
     exports org.bouncycastle.mail.smime;
-    exports org.bouncycastle.mail.smime.examples;
+    //exports org.bouncycastle.mail.smime.examples;
     exports org.bouncycastle.mail.smime.handlers;
     exports org.bouncycastle.mail.smime.util;
     exports org.bouncycastle.mail.smime.validator;
