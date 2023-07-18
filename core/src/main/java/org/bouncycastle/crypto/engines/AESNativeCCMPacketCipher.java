19ca48cff466213ff01490c543055001a320dc84
gefei <gefei.li@keyfactor.com>
2023-07-18 14:07:16 +0930

    Suspend work on native code of CCM packet cipher.

---
+++ core/src/main/java/org/bouncycastle/crypto/engines/AESNativeCCMPacketCipher.java
@@ -0,0 +1,99 @@
+package org.bouncycastle.crypto.engines;
+
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.ExceptionMessage;
+import org.bouncycastle.crypto.PacketCipher;
+import org.bouncycastle.crypto.PacketCipherException;
+import org.bouncycastle.crypto.params.AEADParameters;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+
+public class AESNativeCCMPacketCipher
+    implements PacketCipher
+{
+    @Override
+    public int getOutputSize(boolean encryption, CipherParameters params, int len)
+    {
+        int macSize;
+        if (params instanceof AEADParameters)
+        {
+            AEADParameters param = (AEADParameters)params;
+            macSize = getMacSize(encryption, param.getMacSize());
+        }
+        else if (params instanceof ParametersWithIV)
+        {
+            macSize = 8;
+        }
+        else
+        {
+            throw new IllegalArgumentException("invalid parameters passed to CCM: " + params.getClass().getName());
+        }
+        return getOutputSize(encryption, len, macSize);
+    }
+
+    @Override
+    public int processPacket(boolean forEncryption, CipherParameters params, byte[] input, int inOff, int len, byte[] output, int outOff)
+        throws PacketCipherException
+    {
+        int macSize;
+        byte[] nonce;
+        byte[] initialAssociatedText;
+        byte[] key;
+        KeyParameter keyParam;
+        if (params instanceof AEADParameters)
+        {
+            AEADParameters param = (AEADParameters)params;
+            try
+            {
+                macSize = getMacSize(forEncryption, param.getMacSize());
+            }
+            catch (IllegalArgumentException e)
+            {
+                throw PacketCipherException.from(e);
+            }
+            nonce = param.getNonce();
+            initialAssociatedText = param.getAssociatedText();
+            keyParam = param.getKey();
+        }
+        else if (params instanceof ParametersWithIV)
+        {
+            ParametersWithIV param = (ParametersWithIV)params;
+            macSize = 8;
+            nonce = param.getIV();
+            initialAssociatedText = null;
+            keyParam = (KeyParameter)param.getParameters();
+        }
+        else
+        {
+            throw PacketCipherException.from(new IllegalArgumentException("invalid parameters passed to CCM"));
+        }
+        if (keyParam != null)
+        {
+            key = keyParam.getKey();
+        }
+        else
+        {
+            throw PacketCipherException.from(new IllegalArgumentException("CCM cipher unitialized."));
+        }
+        int iatLen = initialAssociatedText != null ? initialAssociatedText.length : 0;
+        int outLen = output != null ? output.length : 0;
+        int resultLen = processPacket(forEncryption, key, key.length, nonce, nonce.length, initialAssociatedText, iatLen,
+            macSize, input, inOff, len, output, outOff, outLen);
+        return resultLen;
+    }
+
+    static native int getOutputSize(boolean encryption, int len, int macSize);
+
+    static native int processPacket(boolean encryption, byte[] key, int keyLen, byte[] nonce, int nonLen, byte[] aad,
+                                    int aadLen, int macSize, byte[] in, int inOff, int inLen, byte[] out, int outOff, int outLen);
+
+    private int getMacSize(boolean forEncryption, int requestedMacBits)
+    {
+        if (forEncryption && (requestedMacBits < 32 || requestedMacBits > 128 || 0 != (requestedMacBits & 15)))
+        {
+            throw new IllegalArgumentException("tag length in octets must be one of {4,6,8,10,12,14,16}");
+        }
+
+        return requestedMacBits >>> 3;
+    }
+}
---
+++ native_c/intel/jni/ccm_pc_jni.c
@@ -0,0 +1,32 @@
+#include "org_bouncycastle_crypto_engines_AESNativeCCM.h"
+#include "../ccm/ccm.h"
+#include "../../jniutil/exceptions.h"
+#include "../../jniutil/bytearrays.h"
+#include "../../jniutil/jni_asserts.h"
+#include <stdlib.h>
+
+void handle_ccm_result(JNIEnv *env, ccm_err *err) {
+    if (err == NULL) {
+        return;
+    }
+    switch (err->type) {
+        case ILLEGAL_STATE:
+            throw_java_invalid_state(env, err->msg);
+            break;
+        case ILLEGAL_ARGUMENT:
+            throw_java_illegal_argument(env, err->msg);
+            break;
+        case ILLEGAL_CIPHER_TEXT:
+            throw_bc_invalid_ciphertext_exception(env, err->msg);
+            break;
+        case OUTPUT_LENGTH:
+            throw_bc_output_length_exception(env, err->msg);
+            break;
+        default:
+            throw_java_invalid_state(env, "unknown error from ccm");
+            break;
+    }
+
+    ccm_err_free(err);
+
+}
