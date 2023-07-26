77584419259eeb809d5eafb212290e8b6bfdbd3e
mwcw <megan@cryptoworkshop.com>
2023-07-27 08:46:32 +1000

    debugging ci build

--- version.sh
+++ version.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 export script_loc=$(dirname -- $(readlink -f - "$0"))
 
+echo "Script Location: ${script_loc}"
+
+ls -al "${script_loc}"
+
 fgrep version "${script_loc}/gradle.properties" | sed -e "s/version=//"
 
