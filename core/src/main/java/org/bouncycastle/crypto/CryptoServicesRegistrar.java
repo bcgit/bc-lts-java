1a1d333febf9b112e12e7320697d0b4986e89250
mwcw <megan@cryptoworkshop.com>
2023-07-26 22:28:12 +1000

    SNAPSHOT version

--- core/src/main/java/org/bouncycastle/crypto/CryptoServicesRegistrar.java
+++ core/src/main/java/org/bouncycastle/crypto/CryptoServicesRegistrar.java
@@ -149,7 +149,7 @@ public final class CryptoServicesRegistrar
 
     public static String getInfo()
     {
-        return "BouncyCastle APIs (LTS edition) v2.73.3";
+        return "BouncyCastle APIs (LTS edition) v2.73.3-SNAPSHOT";
     }
 
     public static boolean isNativeEnabled()
