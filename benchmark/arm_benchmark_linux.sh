2fc4a5045d91e9ce061953e60b69f43ef63bc6be
mwcw <megan@cryptoworkshop.com>
2023-07-28 10:04:26 +1000

    Added single run all script for arm benchmarking

--- benchmark/arm_benchmark_linux.sh
+++ benchmark/arm_benchmark_linux.sh
@@ -4,11 +4,11 @@ export script_loc=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 
 pushd $script_loc
 
-arm_benchmark_linux_cbc.sh
-arm_benchmark_linux_cfb.sh
-arm_benchmark_linux_ctr.sh
-arm_benchmark_linux_ecb.sh
-arm_benchmark_linux_gcm.sh
-arm_benchmark_linux_sha.sh
+./arm_benchmark_linux_cbc.sh
+./arm_benchmark_linux_cfb.sh
+./arm_benchmark_linux_ctr.sh
+./arm_benchmark_linux_ecb.sh
+./arm_benchmark_linux_gcm.sh
+./arm_benchmark_linux_sha.sh
 
 popd
