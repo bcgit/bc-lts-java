4ba7432f16fdb3ffdff89531d538e8b4ece7dcdb
mwcw <megan@cryptoworkshop.com>
2023-08-11 10:33:47 +1000

    first cut of ci test scripts

---
+++ ci/test_jvms.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+#
+# This script is for running inside the docker container
+#
+
+# Runs ant scripts that target different JVMs.
+
+cd /workspace/bc-lts-java
+source ci/common.sh
+
+
+export JAVA_HOME=`openjdk_11`
+export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$PATH:`ant-bin-1-10`
+
+
+./gradlew clean compileJava
+(cd native_c; ./build_linux.sh;)
+./gradlew clean cleanNative withNative build compileTestJava -x test
+
+ant -f test11.xml
+ant -f test17.xml
+ant -f test21.xml
+
+
