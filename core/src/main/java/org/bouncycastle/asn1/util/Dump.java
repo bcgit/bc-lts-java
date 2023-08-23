cc883e9ef527cf1edc609708f01ecafc9f24d1f6
David Hook <dgh@cryptoworkshop.com>
2023-08-23 17:27:04 +1000

    added stub version of Dump to keep the stats in order.

---
+++ core/src/main/java/org/bouncycastle/asn1/util/Dump.java
@@ -0,0 +1,16 @@
+package org.bouncycastle.asn1.util;
+
+/**
+ * Command line ASN.1 Dump utility.
+ * <p>
+ *     Usage: org.bouncycastle.asn1.util.Dump [-v] ber_encoded_file
+ * </p>
+ * <b>Now a no-op</b>
+ */
+public class Dump
+{
+    public static void main(String args[]) throws Exception
+    {
+
+    }
+}
