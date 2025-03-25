b7c94268be57eee09ab7f87a55aae2fae4801af3
David Hook <dgh@cryptoworkshop.com>
2025-03-25 13:34:57 +1100

    added version support

--- benchmark/benchmark_linux_cfb.sh
+++ benchmark/benchmark_linux_cfb.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+# usage: benchmark_linux_cfb.sh <version>
+# where <version> is one the versions in the bc-lts-java-jars repository
 
+export ltsLib="../../bc-lts-java-jars/$1/bcprov-lts8on-$1.jar"
 
-export ltsLib="../jars/bcprov-lts8on-2.73.0-SNAPSHOT.jar"
-
-#exit;
-
+java -version
 
 java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose
+
+vaesf=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep VAESF | fgrep supported`
+vaes=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep "VAES " | fgrep supported`
 
 ../gradlew clean build
 
 
+if [ -n "$vaesf" ]
+then
+    echo "CFB-NI (JCE VAESF)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
+fi
 
-echo "CFB-NI (JCE VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
-
-echo "CFB-NI (VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
+if [ -n "$vaes" ]
+then
+    echo "CFB-NI (VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
+fi
 
 echo "CFB-NI (avx)"
 java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
@@ -24,4 +32,12 @@ java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/
 echo "CFB-NI (java)"
 java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cfb
 
-java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CFB cfb_report_c.html CFBNative_JCE-vaesf.csv CFBNative_JCE-vaes.csv CFBNative_JCE-avx.csv CFBJava_JCE-java.csv
+if [ -n "$vaesf" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CFB cfb_report_c.html CFBNative_JCE-vaesf.csv CFBNative_JCE-vaes.csv CFBNative_JCE-avx.csv CFBJava_JCE-java.csv
+elif [ -n "$vaes" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CFB cfb_report_c.html CFBNative_JCE-vaes.csv CFBNative_JCE-avx.csv CFBJava_JCE-java.csv
+else
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CFB cfb_report_c.html CFBNative_JCE-avx.csv CFBJava_JCE-java.csv
+fi
