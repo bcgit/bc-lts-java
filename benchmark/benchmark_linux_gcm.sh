18fbb3890c6f15b287dbfa5a8f5547a2b412078b
David Hook <dgh@cryptoworkshop.com>
2025-03-25 13:26:58 +1100

    added version support

--- benchmark/benchmark_linux_gcm.sh
+++ benchmark/benchmark_linux_gcm.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
+# usage: benchmark_linux_gcm.sh <version>
+# where <version> is one the versions in the bc-lts-java-jars repository
 
-export fipsLib="../../bc-lts-java-jars/2.73.2/bcprov-lts8on-2.73.2.jar"
+
+export ltsLib="../../bc-lts-java-jars/$1/bcprov-lts8on-$1.jar"
+
+java -version
+
+java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose
+
+vaesf=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep VAESF | fgrep supported`
+vaes=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep "VAES " | fgrep supported`
 
 ../gradlew clean build
 
-java  -cp "$fipsLib" org.bouncycastle.util.DumpInfo -verbose
+if [ -n "$vaesf" ]
+then
+    echo "GCM-NI (VAESF)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
+fi
 
-echo "GCM-NI (VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
-
-echo "GCM-NI (VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
+if [ -n "$vaes" ]
+then
+    echo "GCM-NI (VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
+fi
 
 echo "GCM-NI (if available)"
- java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
+ java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
 
 echo "GCM-java (if available)"
- java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
+ java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark gcm
 
 
-
-java -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport GCM gcm_report_c.html  GCMNative-avx.csv GCMNative-vaesf.csv GCMNative-vaes.csv GCMJava-java.csv
+if [ -n "$vaesf" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport GCM gcm_report_c.html  GCMNative-avx.csv GCMNative-vaesf.csv GCMNative-vaes.csv GCMJava-java.csv
+elif [ -n "$vaes" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport GCM gcm_report_c.html  GCMNative-avx.csv GCMNative-vaes.csv GCMJava-java.csv
+else
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport GCM gcm_report_c.html  GCMNative-avx.csv GCMJava-java.csv
+fi
