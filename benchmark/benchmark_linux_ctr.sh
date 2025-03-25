0031348dc87ede5e7ef7709c7103cdb170046fa4
David Hook <dgh@cryptoworkshop.com>
2025-03-25 13:09:08 +1100

    add version support, capability check

--- benchmark/benchmark_linux_ctr.sh
+++ benchmark/benchmark_linux_ctr.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
+# usage: benchmark_linux_ctr.sh <version>
+# where <version> is one the versions in the bc-lts-java-jars repository
 
 
-export fipsLib="../jars/bcprov-lts8on-2.73.0-SNAPSHOT.jar"
+export ltsLib="../../bc-lts-java-jars/$1/bcprov-lts8on-$1.jar"
 
-#exit;
+java -version
+
+java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose
 
 
-java  -cp "$fipsLib" org.bouncycastle.util.DumpInfo -verbose
+vaesf=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep VAESF | fgrep supported`
+vaes=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep "VAES " | fgrep supported`
+
 
 ../gradlew clean build
 
 
-echo "CTR-NI (JCE VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+if [ -n "$vaesf" ]
+then
+    echo "CTR-NI (JCE VAESF)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+fi
 
-echo "CTR-NI (VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+if [ -n "$vaes" ]
+then
+    echo "CTR-NI (VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+fi
 
 echo "CTR-NI (avx)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
 
 echo "CTR-NI (java)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
+java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ctr
 
-java -cp "$fipsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CTR ctr_report_c.html CTRNative_JCE-vaesf.csv CTRNative_JCE-vaes.csv CTRNative_JCE-avx.csv CTRJava_JCE-java.csv
+if [ -n "$vaesf" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CTR ctr_report_c.html CTRNative_JCE-vaesf.csv CTRNative_JCE-vaes.csv CTRNative_JCE-avx.csv CTRJava_JCE-java.csv
+elif [ -n "$vaes" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CTR ctr_report_c.html CTRNative_JCE-vaes.csv CTRNative_JCE-avx.csv CTRJava_JCE-java.csv
+else
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CTR ctr_report_c.html CTRNative_JCE-avx.csv CTRJava_JCE-java.csv
+fi
