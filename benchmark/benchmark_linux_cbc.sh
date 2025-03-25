c6247d260fec845fc7c432e3cd3150b0806a3e7a
David Hook <dgh@cryptoworkshop.com>
2025-03-25 12:40:34 +1100

    added use of jars dir, check for vaesf, vaes

--- benchmark/benchmark_linux_cbc.sh
+++ benchmark/benchmark_linux_cbc.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
+# usage: benchmark_linux_cbc.sh <version>
+# where <version> is one the versions in the bc-lts-java-jars repository
 
 
-export ltsLib="../jars/bcprov-lts8on-2.73.0-SNAPSHOT.jar"
+export ltsLib="../../bc-lts-java-jars/$1/bcprov-lts8on-$1.jar"
 
 java -version
 
 java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose
 
+
+vaesf=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep VAESF | fgrep supported`
+vaes=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep "VAES " | fgrep supported`
+
 ../gradlew clean build
 
 
-echo "CBC-NI (JCE VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
+echo $vaesf
+if [ -n "$vaesf" ]
+then
+   echo "CBC-NI (JCE VAESF)"
+   java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
+fi
 
-echo "CBC-NI (VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
+if [ -n "$vaes" ]
+then
+    echo "CBC-NI (VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
+fi
 
 echo "CBC-NI (avx)"
 java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
@@ -22,4 +35,12 @@ java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=avx -cp "$ltsLib:./build/libs/
 echo "CBC-NI (java)"
 java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark cbc
 
-java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CBC cbc_report_c.html CBCNative-vaesf.csv CBCNative-vaes.csv CBCNative-avx.csv CBCJava-java.csv
+if [ -n "$vaesf" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CBC cbc_report_c.html CBCNative-vaesf.csv CBCNative-vaes.csv CBCNative-avx.csv CBCJava-java.csv
+elif [ -n "$vaes" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CBC cbc_report_c.html CBCNative-vaes.csv CBCNative-avx.csv CBCJava-java.csv
+else
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport CBC cbc_report_c.html CBCNative-avx.csv CBCJava-java.csv
+fi
--- benchmark/benchmark_linux_ecb.sh
+++ benchmark/benchmark_linux_ecb.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 
-export ltsLib="../jars/bcprov-lts8on-2.73.0-SNAPSHOT.jar"
+export ltsLib="../../bc-lts-java-jars/$1/bcprov-lts8on-$1.jar"
 
+java -version
 
 java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose
+
+
+vaesf=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep VAESF | fgrep supported`
+vaes=`java  -cp "$ltsLib" org.bouncycastle.util.DumpInfo -verbose | fgrep "VAES " | fgrep supported`
 
 ../gradlew clean build
 
 
+if [ -n "$vaesf" ]
+then
+    echo "ECB-NI (JCE VAESF)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb_jce
+    echo "ECB-NI (VAESF)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb
+fi
 
-echo "ECB-NI (JCE VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb_jce
-
-echo "ECB-NI (VAESF)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaesf -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb
 
 
-echo "ECB-NI (JCE VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb_jce
-
-echo "ECB-NI (VAES)"
-java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb
+if [ -n "$vaes" ]
+then
+    echo "ECB-NI (JCE VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb_jce
+    echo "ECB-NI (VAES)"
+    java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=vaes -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb
+fi
 
 
 echo "ECB-NI (JCE avx)"
@@ -37,4 +46,12 @@ java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs
 echo "ECB-NI (java)"
 java -Xmx2g -Dorg.bouncycastle.native.cpu_variant=java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.Benchmark ecb
 
-java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport ECB ecb_report_c.html ECBNative_JCE-vaesf.csv ECBNative-vaesf.csv  ECBNative_JCE-vaes.csv ECBNative-vaes.csv ECBNative-avx.csv  ECBNative_JCE-avx.csv ECBJava-java.csv
+if [ -n "$vaesf" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport ECB ecb_report_c.html ECBNative_JCE-vaesf.csv ECBNative-vaesf.csv  ECBNative_JCE-vaes.csv ECBNative-vaes.csv ECBNative-avx.csv  ECBNative_JCE-avx.csv ECBJava-java.csv
+elif [ -n "$vaes" ]
+then
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport ECB ecb_report_c.html  ECBNative_JCE-vaes.csv ECBNative-vaes.csv ECBNative-avx.csv  ECBNative_JCE-avx.csv ECBJava-java.csv
+else
+    java -cp "$ltsLib:./build/libs/bc-benchmark-java-0.1.jar" org.bouncycastle.benchmark.CreateReport ECB ecb_report_c.html ECBNative-avx.csv  ECBNative_JCE-avx.csv ECBJava-java.csv
+fi
