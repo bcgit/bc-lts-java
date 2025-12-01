d35039d06026fa6cf15d621f4a09064247d8ecf7
David Hook <dgh@cryptoworkshop.com>
2025-12-02 10:34:16 +1100

    updated index script

---
+++ index-update.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+file_ext=$1
+
+shift 
+
+bc_java_dir="../../bc-java"
+
+for file in $*
+do
+echo $file
+match=`echo $file | egrep "org/bouncycastle/asn1/(cryptlib|edec|gnu|iana|isara|iso|kisa|microsoft|misc|mozilla|nsri|ntt|oiw|rosstandart)"`
+if [  -n "$match" ]
+then
+	loc=`echo $file | sed -e "s/core/util/"`
+	current=`cd $bc_java_dir; sha256sum $loc |  sed -e "s/ .*//"`
+	current=`echo $current | sed -e "s/util/core/"`
+else
+	current=`cd $bc_java_dir; sha256sum $file |  sed -e "s/ .*//"`
+fi
+ed indexes/bc-java.${file_ext}.index << %
+g:$file:s:^.*$:$current  $file:
+w
+q
+%
+done
