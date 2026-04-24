fca2cec47f6732e486ca244183489a1041d548e1
mwcw <megan@cryptoworkshop.com>
2026-04-24 17:15:43 +1000

    Added general test that runs the normal test targets.

---
+++ ci/test_general.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+#
+# This script is for running inside the docker container
+#
+
+cd /workspace/bc-lts-java
+source ci/common.sh
+
+
+export JAVA_HOME=`openjdk_21`
+export PATH=$JAVA_HOME/bin:$PATH
+
+env
+
+
+# Build headers
+./gradlew clean compileJava
+
+(
+  # Compile native code
+ cd native_c
+ ./build_linux.sh
+)
+
+
+#
+# Do a build and run basic test stanzas on one JVM
+# Multi-JVM and variant tasks run later
+#
+./gradlew clean cleanNative withNative build
+
+
