5e212c82b14e184dcbd1a0928b0738db50d4c83c
David Hook <dgh@cryptoworkshop.com>
2026-09-11 23:44:01 +1000

    Declare mail's optional javax.mail/javax.activation module dependency under all four artifact-dependent names (mail, java.mail, activation, java.activation) in module-info.java, matching upstream; add the moduleNameStubJars stub-module-jar mechanism and switch compileJava9Java to source/targetCompatibility=9 (rather than options.release=9) so the java.activation automatic module name resolves without colliding with the platform module of the same name under --release 9

--- mail/src/main/jdk1.9/module-info.java
+++ mail/src/main/jdk1.9/module-info.java
@@ -1,11 +1,17 @@
 module org.bouncycastle.lts.mail
 {
     requires org.bouncycastle.lts.prov;
-    requires java.datatransfer;
     requires transitive org.bouncycastle.lts.pkix;
+    requires java.datatransfer;
+    requires org.bouncycastle.lts.util;
+
+    // optional: javax.mail/javax.activation go by these four names depending on the artifact used, a hard requires on any one of them breaks the rest.
+    requires static mail;
+    requires static java.mail;
+    requires static activation;
+    requires static java.activation;
 
     exports org.bouncycastle.mail.smime;
-    //exports org.bouncycastle.mail.smime.examples;
     exports org.bouncycastle.mail.smime.handlers;
     exports org.bouncycastle.mail.smime.util;
     exports org.bouncycastle.mail.smime.validator;
