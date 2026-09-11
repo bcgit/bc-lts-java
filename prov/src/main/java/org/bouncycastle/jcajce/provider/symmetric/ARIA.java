5e0fe7cf9ed57ff109f255ce0d0f017dcba5b850
David Hook <dgh@cryptoworkshop.com>
2026-09-12 01:18:17 +1000

    prov: harden AES/ARIA/SM4/PBEPBKDF2 GCM/CCM param handling, cap PBKDF2 iterations

--- prov/src/main/java/org/bouncycastle/jcajce/provider/symmetric/ARIA.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/symmetric/ARIA.java
@@ -40,6 +40,7 @@ import org.bouncycastle.jcajce.provider.symmetric.util.BlockCipherProvider;
 import org.bouncycastle.jcajce.provider.symmetric.util.GcmSpecUtil;
 import org.bouncycastle.jcajce.provider.symmetric.util.IvAlgorithmParameters;
 import org.bouncycastle.jcajce.spec.AEADParameterSpec;
+import org.bouncycastle.util.Exceptions;
 
 public final class ARIA
 {
@@ -351,7 +352,14 @@ public final class ARIA
         protected void engineInit(byte[] params)
             throws IOException
         {
-            gcmParams = GCMParameters.getInstance(params);
+            try
+            {
+                gcmParams = GCMParameters.getInstance(params);
+            }
+            catch (RuntimeException e)
+            {
+                throw Exceptions.ioException("Not a valid GCM Parameter encoding.", e);
+            }
         }
 
         protected void engineInit(byte[] params, String format)
@@ -362,7 +370,7 @@ public final class ARIA
                 throw new IOException("unknown format specified");
             }
 
-            gcmParams = GCMParameters.getInstance(params);
+            engineInit(params);
         }
 
         protected byte[] engineGetEncoded()
@@ -436,7 +444,14 @@ public final class ARIA
         protected void engineInit(byte[] params)
             throws IOException
         {
-            ccmParams = CCMParameters.getInstance(params);
+            try
+            {
+                ccmParams = CCMParameters.getInstance(params);
+            }
+            catch (RuntimeException e)
+            {
+                throw Exceptions.ioException("Not a valid CCM Parameter encoding.", e);
+            }
         }
 
         protected void engineInit(byte[] params, String format)
@@ -447,7 +462,7 @@ public final class ARIA
                 throw new IOException("unknown format specified");
             }
 
-            ccmParams = CCMParameters.getInstance(params);
+            engineInit(params);
         }
 
         protected byte[] engineGetEncoded()
