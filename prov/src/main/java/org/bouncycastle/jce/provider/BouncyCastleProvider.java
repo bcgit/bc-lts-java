40f80bb14bfd29ce484b734fbeb9b94a9b6a8467
David Hook <dgh@cryptoworkshop.com>
2026-09-26 16:34:49 +1000

    version: move to 2.73.14-SNAPSHOT.

--- prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
+++ prov/src/main/java/org/bouncycastle/jce/provider/BouncyCastleProvider.java
@@ -151,7 +151,7 @@ public final class BouncyCastleProvider
      */
     public BouncyCastleProvider()
     {
-        super(PROVIDER_NAME, 2.7313, info);
+        super(PROVIDER_NAME, 2.7314, info);
 
         AccessController.doPrivileged(new PrivilegedAction()
         {
